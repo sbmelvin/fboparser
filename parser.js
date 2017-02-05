97987d16ba58c3317b10d844d21b1627eaf83a5b
Stephen Melvin <stephenbmelvin@gmail.com>
2017-02-04 19:59:42 -0600

    Added sqlite3 module. Used presol.js as boilerplate code for other parsers.

--- parser.js
+++ parser.js
@@ -1,34 +1,51 @@
+const presol = require("./presol.js");
 const fs = require('fs');
 const path = require('path');
+const sqlite3 = require('sqlite3').verbose();
+const FBOFEED_DIR = '/Users/stephen/Development/fbo_data/';
 
-const presol = require("./presol.js");
+const db = new sqlite3.Database('contracts.sqlite', (err) => {
+	if (err) throw err;
+	ReadFBOFiles();
+});
+
+
 /*
-const srcsgt = require("srcsgt");
-const snote 	= require("snote");
-const combine = require("combine");
-const amdcss = require("amdcss");
-const mod = require("mod");
-const award = require("award");
-const ja = require("ja");
-const fairopp = require("fairopp");
-const archive = require("archive");
-const unarchive = require("unarchive");
+const srcsgt	= require("srcsgt");
+const snote		= require("snote");
+const combine	= require("combine");
+const amdcss	= require("amdcss");
+const mod		= require("mod");
+const award		= require("award");
+const ja		= require("ja");
+const fairopp	= require("fairopp");
+const archive	= require("archive");
+const unarchive	= require("unarchive");
 */
-
-const FBOFEED_DIR = '/Users/stephen/Development/fbo_data/FBOFeed';
 
 // PARSE PRESOL
 
-const files = fs.readdirSync(FBOFEED_DIR);
-
-files.forEach((file) => {
-	fs.readFile(path.join(FBOFEED_DIR, file), 'utf8', (err, data) => {
-		console.log("Parsing ", file);
+// Get list of files in FBOFEED_DIR
+function ReadFBOFiles() {
+	fs.readdir(FBOFEED_DIR, (err, files) => {
 		if (err) throw err;
 
-		// Parse data
-		let presolRecords = presol.parse(data);
+		// For each file, craft the full path
+		files.forEach((file) => {
+			let filePath = path.join(FBOFEED_DIR, file);
 
-		presolRecords.length? console.log(presolRecords.length) : console.log("No results");
+			// Ensure the file is not a directory
+			fs.stat(filePath, (err, stats) => {
+				if (stats.isDirectory()) return;
+
+				// Read the file and parse its contents
+				let data = fs.readFileSync(filePath, 'utf8');
+
+				if (data) {
+					let presolRecords = presol.parse(data);
+					console.log(filePath, ": ", presolRecords.length);
+				}
+			});
+		});
 	});
-});
+}
