709119c6832e3fd283f282d6b329b0052eac1ded
Stephen Melvin <stephenbmelvin@gmail.com>
2017-02-06 19:41:47 -0600

    added package.json

--- parser.js
+++ parser.js
@@ -1,29 +1,32 @@
-const presol = require("./presol.js");
+const presol	= require("./parsers/presol.js");
+const srcsgt	= require("./parsers/srcsgt");
+const snote		= require("./parsers/snote");
+const combine	= require("./parsers/combine");
+const amdcss	= require("./parsers/amdcss");
+const mod		= require("./parsers/mod");
+const award		= require("./parsers/award");
+const ja		= require("./parsers/ja");
+const fairopp	= require("./parsers/fairopp");
+const archive	= require("./parsers/archive");
+const unarchive	= require("./parsers/unarchive");
+
 const fs = require('fs');
 const path = require('path');
 const sqlite3 = require('sqlite3').verbose();
 const FBOFEED_DIR = '/Users/stephen/Development/fbo_data/';
+//const FBOFEED_DIR = './FBOFEED';
+
+if (fs.existsSync("contracts.sqlite")) {
+	console.log("Removing existing contracts.sqlite file");
+	fs.unlinkSync("contracts.sqlite");
+}
+
 
 const db = new sqlite3.Database('contracts.sqlite', (err) => {
 	if (err) throw err;
+	initPresolDatabase();
 	ReadFBOFiles();
 });
-
-
-/*
-const srcsgt	= require("srcsgt");
-const snote		= require("snote");
-const combine	= require("combine");
-const amdcss	= require("amdcss");
-const mod		= require("mod");
-const award		= require("award");
-const ja		= require("ja");
-const fairopp	= require("fairopp");
-const archive	= require("archive");
-const unarchive	= require("unarchive");
-*/
-
-// PARSE PRESOL
 
 // Get list of files in FBOFEED_DIR
 function ReadFBOFiles() {
@@ -31,21 +34,74 @@ function ReadFBOFiles() {
 		if (err) throw err;
 
 		// For each file, craft the full path
-		files.forEach((file) => {
+		files.forEach(file => {
 			let filePath = path.join(FBOFEED_DIR, file);
 
 			// Ensure the file is not a directory
 			fs.stat(filePath, (err, stats) => {
+				if (err) throw err;
 				if (stats.isDirectory()) return;
 
 				// Read the file and parse its contents
-				let data = fs.readFileSync(filePath, 'utf8');
+				fs.readFile(filePath, 'utf8', (err, data) => {
+					if (err) throw err;
 
-				if (data) {
 					let presolRecords = presol.parse(data);
-					console.log(filePath, ": ", presolRecords.length);
-				}
+					return;
+					db.serialize(function() {
+						var stmt = db.prepare("INSERT INTO PRESOL(date, year, agency, office, location, zip, classcod, naics, offadd, subject, solnbr, respdate, contact) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (err) => {
+							if (err) throw err;
+						});
+						
+						presolRecords.forEach(r => {
+							let values = [
+								r.date,
+								r.year,
+								r.agency,
+								r.office,
+								r.location,
+								r.zip,
+								r.classcod,
+								r.naics,
+								r.offadd,
+								r.subject,
+								r.solnbr,
+								r.respdate,
+								r.contact
+							];
+
+							stmt.run(values, (err) => {
+								if (err) throw err;
+							});
+						});
+
+						stmt.finalize();
+					});
+				});
 			});
 		});
 	});
+}
+
+function initPresolDatabase(callback) {
+	let sql = `CREATE TABLE PRESOL (
+		date		text,
+		year		text,
+		agency		text,
+		office		text,
+		location	text,
+		zip			text,
+		classcod	text,
+		naics		text,
+		offadd		text,
+		subject		text,
+		solnbr		text,
+		respdate	text,
+		contact		text
+		)`;
+
+		db.run(sql, (err) => {
+			if (err) throw err;
+			console.log("Created PRESOL table.");
+		});
 }
--- parsers/presol.js
+++ parsers/presol.js
@@ -5,26 +5,57 @@ module.exports.parse = function(data) {
 
 	let presolRecords = [];
 
-	matches.forEach((match) => {
-		let presolRecord = {};
+	let regex = {
+		date: /<DATE>(\d+)+/,	 
+		year: /<YEAR>(\d)+/,	 
+		agency: /<AGENCY>(.*)/,	 
+		office: /<OFFICE>(.*)/,	 
+		location: /<LOCATION>(.*)/,	 
+		zip: /<ZIP>(.*)/,	 
+		classcod: /<CLASSCOD>(.*)/,	 
+		naics: /<NAICS>(.*)/,	 
+		offadd: /<OFFADD>([^<]*)/,	 
+		subject: /<SUBJECT>(.*)/,	 
+		solnbr: /<SOLNBR>(.*)/,	 
+		respdate: /<RESPDATE>(.*)/,	 
+		contact: /<CONTACT>(.*)/
+	};
 
-		match.split('\n').forEach((line) => {
-			let fields = [
-				{ name: 'date', regex: /<DATE>(\d+)+/ },
-				{ name: 'year', regex: /<YEAR>(\d)+/ },
-				{ name: 'agency', regex: /<AGENCY>(.*)/ },
-				{ name: 'office', regex: /<OFFICE>(.*)/ },
-				{ name: 'location', regex: /<LOCATION>(.*)/ },
-				{ name: 'zip', regex: /<ZIP>(.*)/ },
-				{ name: 'classcod', regex: /<CLASSCOD>(.*)/ },
-				{ name: 'naics', regex: /<NAICS>(.*)/ },
-				{ name: 'offadd', regex: /<OFFADD>([^<]*)/ },
-				{ name: 'subject', regex: /<SUBJECT>(.*)/ },
-				{ name: 'solnbr', regex: /<SOLNBR>(.*)/ },
-				{ name: 'respdate', regex: /<RESPDATE>(.*)/ },
-				{ name: 'contact', regex: /<CONTACT>(.*)/ }];
+	let fields = [
+		{ name: 'date', regex: regex.date}, 
+		{ name: 'year', regex: regex.year}, 
+		{ name: 'agency', regex: regex.agency}, 
+		{ name: 'office', regex: regex.office},
+		{ name: 'location', regex: regex.location},
+		{ name: 'zip', regex: regex.zip},
+		{ name: 'classcod', regex: regex.classcod},
+		{ name: 'naics', regex: regex.naics},
+		{ name: 'offadd', regex: regex.offadd},
+		{ name: 'subject', regex: regex.subject},
+		{ name: 'solnbr', regex: regex.solnbr},
+		{ name: 'respdate', regex: regex.respdate},
+		{ name: 'contact', regex: regex.contact}
+	];
 
-			fields.forEach((field) => {
+	matches.forEach( match => {
+		let presolRecord = {
+			date: '',
+			year: '',
+			agency: '',
+			office: '',
+			location: '',
+			zip: '',
+			classcod: '',
+			naics: '',
+			offadd: '',
+			subject: '',
+			solnbr: '',
+			respdate: '',
+			contact: ''
+		};
+
+		match.split('\n').forEach( line => {
+			fields.forEach( field => {
 				let fieldMatch = field.regex.exec(line);
 				if (fieldMatch) presolRecord[field.name] = fieldMatch[1];
 			});
